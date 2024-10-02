0e20c86696cf46986f4714b2705c5b70fff71f85
OxxxyL <slava.uvarovskiy@mail.ru>
2024-10-02 15:36:24 +0300

    ggg

---
+++ 29.08.py
@@ -0,0 +1,16 @@
+print(2.0 + 2.0) # Привет +-/%
+print(type(2.0))
+
+print("Hello, world!")
+print(type("Hello, World!")) # // целочисленное деление
+
+print('Hello ' + 'world!')       #concatenate
+
+print(type(True), type(False))   #boolean
+
+print (5 > 10, 10 > 5 and 5 > 10)
+print (5 == 5 or 5 > 10)
+
+print(type(int('5')))
+print(type(str('5')))
+print(type(bool('5')))
